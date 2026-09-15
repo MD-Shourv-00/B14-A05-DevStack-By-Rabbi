60968613e2d0239e9d55feb1b5e4e69dfad53488
Shourov-Islam <mrtrepolr44@gmail.com>
2026-09-15 20:29:30 +0600

    Nav bar add and some package like daysi ui and tailwind added

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,22 @@
+import Nav from "./components/Nav";
+import type { techDataType } from "./type/type";
+
+
+const techDatafetch = async (): Promise<techDataType[]> => {
+  const res = await fetch("/data.json")
+  const data = res.json()
+
+  return data;
+}
+
+
 function App() {
+  const techDataPromise = techDatafetch();
+  console.log(techDataPromise)
+
   return (
     <>
-      
+      <Nav />
     </>
   )
 }
